d5231f6d91eda6f2fbe7b6089159aab3372edbeb
agavirja <32424558+agavirja@users.noreply.github.com>
2024-09-11 08:22:06 -0500

    actualziar app

--- Home.py
+++ Home.py
@@ -1,78 +1,78 @@
 import streamlit as st
-import pandas as pd
-import geopandas as gpd
-import mysql.connector as sql
+from random import random
 import folium
-import plotly.express as px
-from sqlalchemy import create_engine, types
-from streamlit_folium import folium_static
-from bs4 import BeautifulSoup
-from shapely.geometry import Point
+from streamlit_folium import st_folium
 
-st.set_page_config(layout="wide")
+
+def create_marker():
+    random_lat = (0.5 - random()) + st.session_state.map_config["center"][0]
+    random_lon = (0.5 - random()) + st.session_state.map_config["center"][1]
+
+    return folium.Marker(
+        location=[random_lat, random_lon],
+        popup=f"Random marker at {random_lat:.2f}, {random_lon:.2f}",
+    )
+
+
+@st.cache_data
+def base_map():
+    m = folium.Map(
+        location=st.session_state.map_config["center"],
+        zoom_start=st.session_state.map_config["zoom"],
+    )
+    fg = folium.FeatureGroup(name="Markers")
+
+    return m, fg
+
+
+@st.experimental_fragment(run_every=0.5)
+def draw_map():
+    m, fg = base_map()
+
+    for marker in st.session_state["markers"]:
+        fg.add_child(marker)
+
+    st_folium(
+        m,
+        feature_group_to_add=fg,
+        center=st.session_state.map_config["center"],
+        zoom=st.session_state.map_config["zoom"],
+        key="user-map",
+        returned_objects=[],
+        use_container_width=True,
+        height=300,
+    )
+
+
+def add_random_marker():
+    st.session_state["markers"].append(create_marker())
 
 
 def main():
-    mapwidth = 1900
-    height   = 500
-    
-    col1, col2, col3 = st.columns([6,1,1])
-    with col2:
-        st.image('https://ingeurbe.com/wp-content/uploads/elementor/thumbs/LOGO-39-ANOS_v2_ing-1-1-qtuw7n0byqtxcj89rf8jqp43j6zlaj8lx4y9j36ups.png', width=400)
+    st.set_page_config(layout="wide")
 
-    with st.spinner('Cargando información'):
-        data_base, data_activos, data_vehiculos, data_lasttrans, data_anotaciones = getdata()
+    if "markers" not in st.session_state:
+        st.session_state["markers"] = []
 
+    if "map_config" not in st.session_state:
+        st.session_state.map_config = {"center": [4.62, -74.06], "zoom": 8}
 
-    if data_base is not None:
-        col1, col2 = st.columns(2)
-        with col2:
-            m = create_map(data_activos)
-            folium_static(m, width=int(mapwidth*0.7), height=height)
-                
+    st.header("Refreshing map", divider="rainbow")
+    left_column, right_column = st.columns([1, 2])
 
+    with left_column:
+        if st.button("Add random marker"):
+            add_random_marker()
 
-@st.cache_data(show_spinner=False)
-def getdata():
-    try:
-        user     = st.secrets["user_bigdata"]
-        password = st.secrets["password_bigdata"]
-        host     = st.secrets["host_bigdata_lectura"]
-        engine   = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/urbex')
-        
-        data_base        = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_base" , engine)
-        data_activos     = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_activos" , engine)
-        data_vehiculos   = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_vehiculos" , engine)
-        data_lasttrans   = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_lasttrans" , engine)
-        data_anotaciones = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_anotaciones" , engine)
-        engine.dispose()
-        return data_base, data_activos, data_vehiculos, data_lasttrans, data_anotaciones
-    except Exception as e:
-        st.error(f"Error al cargar datos: {str(e)}")
-        return None, None, None, None, None
+        if st.button("Clear markers"):
+            st.session_state["markers"].clear()
 
-@st.cache_data(show_spinner=False)
-def geopoints(data):
-    if data is None or data.empty:
-        return pd.DataFrame().to_json()
-    
-    if 'latitud' in data and 'longitud' in data:
-        data = data[(data['latitud'].notnull()) & (data['longitud'].notnull())]
-        
-    if not data.empty:
-        data['geometry'] = data.apply(lambda x: Point(x['longitud'], x['latitud']), axis=1)
-        data = gpd.GeoDataFrame(data, geometry='geometry')
-        return data.to_json()
-    
-    return pd.DataFrame().to_json()
+        if st.toggle("Start adding markers automatically"):
+            st.experimental_fragment(add_random_marker, run_every=1.0)()
 
-def create_map(data_activos):
-    m = folium.Map(location=[4.687103, -74.058094], zoom_start=12, tiles="cartodbpositron")
-    #if data_activos is not None and not data_activos.empty:
-    #    datagjson = geopoints(data_activos)
-    #    folium.GeoJson(datagjson, name="geojson").add_to(m)
-    #folium.LayerControl().add_to(m)
-    return m
+    with right_column:
+        draw_map()
+
 
 if __name__ == "__main__":
     main()
