916c22f6f2540fea3d35daa516f10e8be096cc12
agavirja <32424558+agavirja@users.noreply.github.com>
2024-09-11 07:19:06 -0500

    crear nuevo codigo

--- Home.py
+++ Home.py
@@ -4,69 +4,72 @@ import geopandas as gpd
 import mysql.connector as sql
 import folium
 import plotly.express as px
-from sqlalchemy import create_engine , types
-from streamlit_folium import st_folium
+from sqlalchemy import create_engine, types
+from streamlit_folium import folium_static
 from bs4 import BeautifulSoup
 from shapely.geometry import Point
-#from streamlit_js_eval import streamlit_js_eval
 
 st.set_page_config(layout="wide")
 
-
 def main():
-    
     mapwidth = 1900
     height   = 500
     
-    col1,col2,col3 = st.columns([6,1,1])
+    col1, col2, col3 = st.columns([6,1,1])
     with col2:
-        st.image('https://ingeurbe.com/wp-content/uploads/elementor/thumbs/LOGO-39-ANOS_v2_ing-1-1-qtuw7n0byqtxcj89rf8jqp43j6zlaj8lx4y9j36ups.png',width=400)
+        st.image('https://ingeurbe.com/wp-content/uploads/elementor/thumbs/LOGO-39-ANOS_v2_ing-1-1-qtuw7n0byqtxcj89rf8jqp43j6zlaj8lx4y9j36ups.png', width=400)
 
-    col1,col2 = st.columns(2)
     with st.spinner('Cargando información'):
-        data_base,data_activos,data_vehiculos,data_lasttrans,data_anotaciones = getdata()
+        data_base, data_activos, data_vehiculos, data_lasttrans, data_anotaciones = getdata()
 
-        with col2:
-            m = folium.Map(location=[4.687103, -74.058094], zoom_start=12,tiles="cartodbpositron")
-            if not data_activos.empty:
-                datagjson = geopoints(data_activos)
-                folium.GeoJson(datagjson).add_to(m)
-            st_map = st_folium(m,width=int(mapwidth*0.7),height=500)
+        if data_base is not None:
+            col1, col2 = st.columns(2)
+            with col2:
+                m = create_map(data_activos)
+                folium_static(m, width=int(mapwidth*0.7), height=height)
+        else:
+            st.error("No se pudieron cargar los datos. Por favor, intente nuevamente más tarde.")
 
 @st.cache_data(show_spinner=False)
 def getdata():
-    
-    user     = st.secrets["user_bigdata"]
-    password = st.secrets["password_bigdata"]
-    host     = st.secrets["host_bigdata_lectura"]
-    engine   = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/urbex')
-    
-    data_base        = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_base" , engine)
-    data_activos     = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_activos" , engine)
-    data_vehiculos   = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_vehiculos" , engine)
-    data_lasttrans   = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_lasttrans" , engine)
-    data_anotaciones = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_anotaciones" , engine)
-
-    engine.dispose()
-
-    return data_base,data_activos,data_vehiculos,data_lasttrans,data_anotaciones
+    try:
+        user     = st.secrets["user_bigdata"]
+        password = st.secrets["password_bigdata"]
+        host     = st.secrets["host_bigdata_lectura"]
+        engine   = create_engine(f'mysql+mysqlconnector://{user}:{password}@{host}/urbex')
+        
+        data_base        = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_base" , engine)
+        data_activos     = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_activos" , engine)
+        data_vehiculos   = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_vehiculos" , engine)
+        data_lasttrans   = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_lasttrans" , engine)
+        data_anotaciones = pd.read_sql_query("SELECT * FROM  urbex.ingeurbe_data_anotaciones" , engine)
+        engine.dispose()
+        return data_base, data_activos, data_vehiculos, data_lasttrans, data_anotaciones
+    except:
+        return None, None, None, None, None
 
 @st.cache_data(show_spinner=False)
 def geopoints(data):
+    if data is None or data.empty:
+        return pd.DataFrame().to_json()
     
-    geojson = pd.DataFrame().to_json()
-    
-    if not data.empty:
-        if 'latitud' in data and 'longitud' in data:
-            data = data[(data['latitud'].notnull()) & (data['longitud'].notnull())]
-            
-    if not data.empty:
-        data['geometry'] = data.apply(lambda x: Point(x['longitud'],x['latitud']),axis=1)
-        data             = gpd.GeoDataFrame(data, geometry='geometry')
-        data             = data[['geometry']]
-        geojson          = data.to_json()
+    if 'latitud' in data and 'longitud' in data:
+        data = data[(data['latitud'].notnull()) & (data['longitud'].notnull())]
         
-    return geojson
+    if not data.empty:
+        data['geometry'] = data.apply(lambda x: Point(x['longitud'], x['latitud']), axis=1)
+        data = gpd.GeoDataFrame(data, geometry='geometry')
+        data = data[['geometry']]
+        return data.to_json()
+    
+    return pd.DataFrame().to_json()
+
+def create_map(data_activos):
+    m = folium.Map(location=[4.687103, -74.058094], zoom_start=12, tiles="cartodbpositron")
+    if data_activos is not None and not data_activos.empty:
+        datagjson = geopoints(data_activos)
+        folium.GeoJson(datagjson).add_to(m)
+    return m
 
 if __name__ == "__main__":
     main()
